42b7d646cb9f0c0d396e80fe810e3120bf6e8fa2
shieldgenerator7 <shieldgenerator7@yahoo.com>
2017-01-21 03:03:15 -0500

    Fixed #10 Clickable regions added
    New Current Options for controls:
    1. pressing mouse's keyboard key
    2. Clicking in a mouse's row
    3. Tapping a touchscreen in a mouse's row

--- Assets/Script/MouseController.cs
+++ Assets/Script/MouseController.cs
@@ -7,6 +7,8 @@ public class MouseController : MonoBehaviour {
     public GameObject toyPrefab;//the prefab for the toy this mouse spawns
     public int laneId;//the id of the lane this mouse occupies
     public float toySpawnDelay = 0.5f;//the minimum time between toy spawns (seconds)
+    public string buttonName;//the name of the button that controls this mouse
+    public GameObject tapArea;//the tap area that this mouse responds to
 
     private float nextToySpawn = 0;//the soonest this mouse can spawn a toy
 
@@ -17,7 +19,30 @@ public class MouseController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButton("NoteE"))
+        bool buttonInput = Input.GetButton(buttonName);
+        bool mouseInput = false;
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 camPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (tapArea.GetComponent<BoxCollider2D>().OverlapPoint(camPoint))
+            {
+                mouseInput = true;
+            }
+        }
+        bool touchInput = false;
+        if (Input.touchCount > 0)
+        {
+            for (int i=0; i<Input.touchCount; i++)
+            {
+                Vector3 camPoint = Camera.main.ScreenToWorldPoint(Input.GetTouch(i).position);
+                if (tapArea.GetComponent<BoxCollider2D>().OverlapPoint(camPoint))
+                {
+                    touchInput = true;
+                    break;
+                }
+            }
+        }
+		if (buttonInput || mouseInput || touchInput)
         {
             if (nextToySpawn < Time.time)
             {
