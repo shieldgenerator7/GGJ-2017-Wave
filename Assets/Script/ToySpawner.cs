3f41055f4bdf7f5bee2a7bf21f5536de0a4321b8
shieldgenerator7 <shieldgenerator7@yahoo.com>
2017-01-22 10:21:25 -0500

    Added more toys

--- Assets/Script/ToySpawner.cs
+++ Assets/Script/ToySpawner.cs
@@ -8,11 +8,16 @@ public class ToySpawner : MonoBehaviour
     //Toy Prefabs
     public GameObject toyPrefab;
     public Sprite toy_bear;
-    public Sprite toy_tuna;
+    public Sprite toy_bunny;
+    public Sprite toy_butterfly;
     public Sprite toy_duck;
+    public Sprite toy_fish;
     public Sprite toy_milk;
+    public Sprite toy_mouse;
+    public Sprite toy_tuna;
+    public Sprite toy_yarn;
     private List<Sprite> toySpriteList;
-    private static int TOY_AMOUNT = 4;
+    private static int TOY_AMOUNT = 9;
 
     private static ToySpawner instance;
 
@@ -21,9 +26,14 @@ public class ToySpawner : MonoBehaviour
         instance = this;
         toySpriteList = new List<Sprite>();
         toySpriteList.Add(toy_bear);
-        toySpriteList.Add(toy_tuna);
+        toySpriteList.Add(toy_bunny);
+        toySpriteList.Add(toy_butterfly);
         toySpriteList.Add(toy_duck);
+        toySpriteList.Add(toy_fish);
         toySpriteList.Add(toy_milk);
+        toySpriteList.Add(toy_mouse);
+        toySpriteList.Add(toy_tuna);
+        toySpriteList.Add(toy_yarn);
     }
 	
 	// Update is called once per frame
@@ -37,6 +47,7 @@ public class ToySpawner : MonoBehaviour
         ToyChecker tc = newToy.GetComponent<ToyChecker>();
         tc.init(startPos);
         tc.GetComponent<Rigidbody2D>().AddForce(instance.toySpawnVelocity);
+        Random.seed = (int)(startPos.y + Time.time);
         int randSprite = Random.Range(0, TOY_AMOUNT);
         newToy.GetComponent<SpriteRenderer>().sprite = instance.toySpriteList[randSprite];
     }
